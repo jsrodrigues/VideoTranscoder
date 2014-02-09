fbfc1474ecae7b2b582189f33a34d16d69eceed4
John Rodrigues <john.rodrigues.biz@gmail.com>
2014-02-08 22:15:24 -0600

    Modified the sample properties file

--- resources/properties.clj
+++ resources/properties.clj
@@ -1,11 +1,10 @@
-(let [props {:dest-dir "C:/Users/jsrodrigues/VirtualMachines/shared/vts/workspace"
-             :ffmpeg-command "C:/FFMpeg-64bit/bin/ffmpeg.exe -i %s -y -vcodec mpeg4 -acodec aac -strict -2 %s/%s.mp4"
-             :activemq-url "tcp://research-dev.dgs.dgsystems.com:61616?jms.prefetchPolicy.queuePrefetch=1"
+(let [props {:dest-dir "<path to destination folder>"
+             :ffmpeg-command "ffmpeg -i %s -y -vcodec mpeg4 -acodec aac -strict -2 %s/%s.mp4"
+             :activemq-url "tcp://localhost:61616?jms.prefetchPolicy.queuePrefetch=1"
              :queue-name "vts"
              :service-port 9095
              :max-n 5
-             :source-file "\\\\cifs.irvqafs1.qa.dgsystems.com\\mediadmz\\2014\\01\\23\\adhoc01227ph_072500\\adhoc01227ph_720p_20140123012236.mpg"
-             :transcoder-agents ["research-dev.dgs.dgsystems.com:9095"]
+             :source-file "<source file location>/some-file.mpeg"
+             :transcoder-agents ["localhost:9096"]
              :concurrent-processes 4}]
   props)
-
